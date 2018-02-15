b3b23e1a7d93e53d941ebe59a6c2c7200e52ecd3
matty9090 <lowe.matthew@mail.com>
2018-02-15 22:08:17 +0000

    Added player health
    + method for damage

--- Player.cpp
+++ Player.cpp
@@ -1,6 +1,6 @@
 #include "Player.hpp"
 
-Player::Player(std::string name) : m_Name(name)
+Player::Player(std::string name) : m_Name(name), m_Health(m_kMaxHealth)
 {
 	
 }
--- Player.hpp
+++ Player.hpp
@@ -16,10 +16,16 @@ class Player
 
 		void AddCardToDeck(CardPtr card) { m_Deck.push_back(card); }
 
+		int GetHealth() { return m_Health; }
+		void TakeDamage(int damage) { m_Health -= damage; }
+
 		std::vector<CardPtr> GetDeck() const { return m_Deck; }
 		std::vector<CardPtr> GetHand() const { return m_Hand; }
 
 	private:
+		const int m_kMaxHealth = 30;
+		int m_Health;
+
 		std::string m_Name;
 		std::vector<CardPtr> m_Deck;
 		std::vector<CardPtr> m_Hand;
